3a26c8156790749edf44aa6a31c246db356921b9
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-27 16:25:21 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,23 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+	boolean IsGiral = false;
+	boolean IsBoy = false; 
+	double r = Math.random();
+	int count = 0;
+	while (IsGiral== false || IsBoy ==false){
+		if (r>0.5){ //if the random number generated is more then 0.5 it's a boy
+			System.out.print("b ");
+			IsBoy = true;
+		}
+		else{
+			System.out.print("g ");
+			IsGiral = true;
+		}
+		count++;
+		r = Math.random();
+	}
+	System.out.println();
+	System.out.println("You made it... and you now have "+count + " children");
 	}
 }
