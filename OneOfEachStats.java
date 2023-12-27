e37e6110bbf54346901e26b0cbe8119ca1e08d4b
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-27 19:33:41 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -12,16 +12,20 @@ public class OneOfEachStats {
 	int T = Integer.parseInt(args[0]); //number of expiremnts
 	int seed = Integer.parseInt(args[1]);
 	Random random = new Random(seed);
+
 	int count2 = 0; 
 	int count3 = 0; 
 	int count4ormore = 0;
 	double avgOfKids = 0;
+	
 	for (int i =0; i<T; i++){
 		boolean IsGiral = false;
 		boolean IsBoy = false; 
-		double randomNumber = random.nextDouble();
+		double randomNumber;
+	
 		int count = 0;
 		while (IsGiral== false || IsBoy ==false){
+			randomNumber = random.nextDouble();
 		if (randomNumber>0.5){ //if the random number generated is more then 0.5 it's a boy
 			//System.out.print("b ");
 			IsBoy = true;
@@ -31,10 +35,11 @@ public class OneOfEachStats {
 			IsGiral = true;
 		}
 		count++;
-		randomNumber = random.nextDouble();
 	
 	}
+	System.out.println("number of kids in " + i + "familiy " + count);
 	avgOfKids += (double) count;
+	//System.out,println("");
 	if (count ==2){
 			count2++;
 		}
@@ -45,16 +50,18 @@ public class OneOfEachStats {
 			count4ormore++; 
 		}
 	}
+	//out of for
 	int mostCommon = 0;
 	if(count3 < count2){
 		mostCommon = 2; 
 	}
-	else if (count3 >count2 && count3 > count4ormore){
+	if (count3 >count2 && count3 > count4ormore){
 		mostCommon = 3; 
 	}
-	else if (count4ormore >count2 && count4ormore >count3){
-		mostCommon = 4; 
+	else {
+		mostCommon = 4;
 	}
+	
 	System.out.println("Average: " +avgOfKids/T + " to get at least one of each gender.");
 	System.out.println("Number of families with 2 children: " + count2);
 	System.out.println("Number of families with 3 children: " + count3);
