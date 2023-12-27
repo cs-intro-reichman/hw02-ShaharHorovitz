4ec941e37b6195eec6a14474186ad43d0dbaa8c8
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-27 20:30:25 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -62,10 +62,10 @@ public class OneOfEachStats {
 	}
 	
 	System.out.println("Average: " +avgOfKids/T + " to get at least one of each gender.");
-	System.out.println("Number of families with 2 children: " + count2 + ".");
-	System.out.println("Number of families with 3 children: " + count3+ ".");
-	System.out.println("Number of families with 4 or more children: " + count4ormore+ ".");
-	System.out.println("The most common number of children is " + mostCommon);
+	System.out.println("Number of families with 2 children: " + count2);
+	System.out.println("Number of families with 3 children: " + count3);
+	System.out.println("Number of families with 4 or more children: " + count4ormore);
+	System.out.println("The most common number of children is " + mostCommon+ ".");
 
 }
 }
