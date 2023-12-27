37a5ca467c6d4b34c5e15aca5ec05f13472d748c
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-27 16:57:33 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -9,11 +9,68 @@ import java.util.Random;
  */
 public class OneOfEachStats {
 	public static void main (String[] args) {
+		import java.util.Random;
+public class OneOfEachStats{
+public static void main(String[] args) {
+int T = Integer.parseInt(args[0]); //number of expiremnts
+int seed = Integer.parseInt(args[1]);
+Random random = new Random(seed);
+int count2 = 0; 
+int count3 = 0; 
+int count4ormore = 0;
+double avgOfKids = 0;
+for (int i =0; i<T; i++){
+	boolean IsGiral = false;
+	boolean IsBoy = false; 
+	double randomNumber = random.nextDouble();
+	int count = 0;
+	while (IsGiral== false || IsBoy ==false){
+	if (randomNumber>0.5){ //if the random number generated is more then 0.5 it's a boy
+		//System.out.print("b ");
+		IsBoy = true;
+	}
+	else{
+		//System.out.print("g ");
+		IsGiral = true;
+	}
+	count++;
+	randomNumber = random.nextDouble();
+
+}
+avgOfKids += (double) count;
+if (count ==2){
+		count2++;
+	}
+if (count ==3){
+		count3++;
+	}
+if(count >= 4) {
+		count4ormore++; 
+	}
+}
+int mostCommon = 0;
+if(count2 > count3 && count2>count4ormore){
+	mostCommon = 2; 
+}
+if (count3 >count2 && count3 > count4ormore){
+	mostCommon = 3; 
+}
+if (count4ormore >count2 && count4ormore >count3){
+	mostCommon = 4; 
+}
+System.out.println("Average: " +avgOfKids/T + " to get at least one of each gender.");
+System.out.println("Number of families with 2 children: " + count2);
+System.out.println("Number of families with 3 children: " + count3);
+System.out.println("Number of families with 4 or more children: " + count4ormore);
+
+}
+}
+
 		// Gets the two command-line arguments
-		int T = Integer.parseInt(args[0]);
-		int seed = Integer.parseInt(args[1]);
+		//int T = Integer.parseInt(args[0]);
+		//int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+        	//Random generator = new Random(seed);  
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
