76ef43bbbf23dcc1908aad1b1caded39079c1e78
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-27 15:53:58 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,14 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+	String word = args[0];
+	String reverseWord = "";
+	int midword = word.length()/2 -1;
+	for (int i=word.length()-1; i>=0; i--){
+	reverseWord = reverseWord + word.charAt(i);
+	}
+	System.out.println(reverseWord);
+	int middleChar = word.length()/2;
+	System.out.println("The middle charcter is: " + word.charAt(middleChar));
 	}
 }
