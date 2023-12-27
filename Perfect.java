ec61af11f754f44754005eb7cb7a6f1beb04da37
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-27 16:21:49 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,26 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+	int num = Integer.parseInt(args[0]); 
+	int count = 0;
+	String exercise = " = ";
+	for (int i =1; i<num; i++ ){
+		if(num%i==0){
+			if(count ==0){
+				exercise+= i;
+				count+=i;
+			}
+			else {
+				count+=i;
+				exercise += " + " + i; 
+			}
+			
+		}
 	}
+	if(count ==num){
+		System.out.println(num +" is a perfect number since "+ num + exercise);
+	}
+	else 
+		System.out.println(num + " is not a perfect number");
+		}
 }
