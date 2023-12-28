80d8c528243fefc9c7bc90127675866fed1f860f
ShaharHorovitz <154262390+ShaharHorovitz@users.noreply.github.com>
2023-12-28 08:18:47 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,14 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 	int num = Integer.parseInt(args[0]); 
 	for(int i=num-1; i>=0; i--){
-	if (i%2==0){
-		System.out.print(" ");
+	
 	}
 	for(int s = 0;s<num;s++){
+		if (i%2==0)
 		System.out.print("* ");
+		else {
+			System.out.print(" *");
+		}
 	}
 	System.out.println();
 	}
