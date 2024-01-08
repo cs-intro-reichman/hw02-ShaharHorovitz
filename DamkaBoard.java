c39127665f8699de2908fc811c261150dec69cbc
Ilia Kohanovski <adsurbum@gmail.com>
2024-01-08 18:14:01 +0200

    Review See TODO comments in the code You should put proper identations in your code! Readability of the code is important Also put attention on proper place of curle braces "}" Read the style guide from the moodle: https://docs.google.com/document/d/178R1ZbXLUT6myw2JjPhYRTL3mfb6oNSe/edit

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,16 +3,19 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
+		// TODO: ident all the code below the main
 	int num = Integer.parseInt(args[0]); 
 	for(int i=0; i<num; i++){
 		for(int s = 1;s<=num;s++){
+			// TODO: ident this piece of code to the right
 		if (i%2==0){
-		System.out.print("* ");
+		System.out.print("* "); // TODO: ident this line to the right
 		}
 		else {
 			System.out.print(" *");
 		}
 		}
+			//
 		System.out.println();
 	}
 	
